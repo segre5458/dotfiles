5bb51dbcd1c7ef68b395a61f86098b48267ce094
segre <segrehouck5458@gmail.com>
2022-12-08 13:53:38 +0900

    change setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf dotfiles/.vimrc ~/.vimrc
-ln -sf dotfiles/.vim ~/.vim
-ln -sf dotfiles/tmux ~/.config/tmux
-ln -sf dotfiles/.zshrc ~/.zshrc
-ln -sf dotfiles/.zprofile ~/.zprofile
-ln -sf dotfiles/.xbindkeysrc ~/.xbindkeysrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/tmux ~/.config/tmux
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
