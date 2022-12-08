231a003ce6acef476c68b20bece44ea001ed3f8f
segre <segrehouck5458@gmail.com>
2022-12-08 17:44:48 +0900

    add file exist check

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/tmux ~/.config/tmux
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
+if [ ! -e ~/.vimrc ]; then
+  ln -s ~/dotfiles/.vimrc ~/.vimrc
+fi
+
+if [ ! -d ~/.vim ]; then
+  ln -s ~/dotfiles/.vim ~/.vim
+fi
+
+if [ ! -d ~/.config/tmux ]; then
+  ln -s ~/dotfiles/tmux ~/.config/tmux
+fi
+
+if [ ! -e ~/.zplug ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+if [ ! -d ~/bin/z ]; then
+  git clone git@github.com:rupa/z.git ~/bin/z
+fi
+if [ ! -e ~/.zshrc ]; then
+  ln -s ~/dotfiles/.zshrc ~/.zshrc
+fi
+
+if [ ! -e ~/.zprofile ]; then
+  ln -s ~/dotfiles/.zprofile ~/.zprofile
+fi
+
+if [ ! -e ~/.xbindkeysrc ]; then
+  ln -s ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
+fi
